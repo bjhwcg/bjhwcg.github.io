ca1e8f3e9f8dfad62dae1c0d77ceb84f1de830b9
SunnSta <mengmengzjxs@163.com>
2018-04-26 00:30:01 +0800

    finish about.html

---
+++ assets/scripts/about.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    //菜单栏头像消失
+	$("#head-picture").animate({ width: "0", opacity: "0", left: "50%" }, "slow");
+	//正文头像出现
+	$("#about-head-picture").animate({ width: "200px", opacity: "1" ,top:"0"}, "slow");
+	//禁止点击about
+    $("#about-button").removeAttr("href");
+	$("#about-button")[0].style.backgroundColor="#FFFFFF";
+	$("#about-button")[0].style.color="#000000"
+}
